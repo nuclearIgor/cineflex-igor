c0bdca40e5a82871333f671655fcc5aa2b5793e0
nuclearIgor <igorflribeiro@gmail.com>
2022-06-17 13:59:32 -0300

    pagina home com redirects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Router from "./routes";
+
 function App() {
   return (
     <div className="App">
-    
+      <Router/>  
     </div>
   );
 }
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import {Link} from 'react-router-dom'
+import './home.css'
+
+export const Home = () => {
+
+    const [movies, setMovies] = useState([])
+
+    async function fetchMovies(){        
+        const res = await fetch('https://mock-api.bootcamp.respondeai.com.br/api/v2/cineflex/movies')
+        setMovies(await res.json())
+        return
+    }
+
+    useEffect(() => {
+        fetchMovies()
+    }, [])
+
+
+    return (
+        <>
+        {/* header */}
+        <h1>Selecione o filme</h1>
+        <main>
+        {
+             Array.isArray(movies) && movies.map(movie => (
+                <div key={movie.id}>
+                <Link to={`/sessoes/${movie.id}`}>
+                    <img src={movie.posterURL} alt="movie poster"/>
+                </Link>
+                </div>
+             ))
+            
+        }
+        </main>
+        </>
+    )
+
+}
